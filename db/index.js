789b9c97883c56ac9b48181a8f9fbe9e0038a336
joshuawc <joshkosh99@gmail.com>
2021-10-29 11:54:21 +1300

    Fix pathing issue for database

--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
+const config = require('./knexfile').development
 const database = require('knex')(config)
 
 module.exports = {
