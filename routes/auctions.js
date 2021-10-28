f88dc891098b702381c2f65c39da840a3e9a807a
tane-git <tanelives@gmail.com>
2021-10-29 08:31:26 +1300

    add seed files and start working on homepage /auctions/ route

--- routes/auctions.js
+++ routes/auctions.js
@@ -2,3 +2,17 @@ const express = require('express')
 const router = express.Router()
 module.exports = router
 
+const db = require('../db/index')
+
+// GET /auctions/
+router.get('/', (req, res) => {
+  db.getAuctions()
+    .then(auctions => {
+      // unsure about how to assign this (destructure? array[0] etc)
+      const viewData = auctions
+
+      // res.render('home', viewData)
+      res.render('home')
+    })
+})
+
