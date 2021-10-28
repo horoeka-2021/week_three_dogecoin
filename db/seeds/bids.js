7fd07f0a360378dde5bf4f511f30a105f5fbe624
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-10-29 11:35:19 +1300

    Create Seed data, Update Gitignore for db

--- db/seeds/bids.js
+++ db/seeds/bids.js
@@ -1,12 +1,12 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('bids').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
+      return knex('bids').insert([
+        { id: 1, auction_id: 1, user_id: 1, amount: 5 },
+        { id: 2, auction_id: 2, user_id: 2, amount: 3.50 },
+        { id: 3, auction_id: 3, user_id: 3, amount: 2.25 }
       ])
     })
 }
