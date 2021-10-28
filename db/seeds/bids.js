1944bed9ccfc968f27d911629dad03bffa4ab82e
joshuawc <joshkosh99@gmail.com>
2021-10-29 12:48:46 +1300

    Plug db functions into initial routes

--- db/seeds/bids.js
+++ db/seeds/bids.js
@@ -6,7 +6,8 @@ exports.seed = function (knex) {
       return knex('bids').insert([
         { id: 1, auction_id: 1, user_id: 1, amount: 5 },
         { id: 2, auction_id: 2, user_id: 2, amount: 3.50 },
-        { id: 3, auction_id: 3, user_id: 3, amount: 2.25 }
+        { id: 3, auction_id: 3, user_id: 3, amount: 2.25 },
+        { id: 4, auction_id: 1, user_id: 1, amount: 10 }
       ])
     })
 }
