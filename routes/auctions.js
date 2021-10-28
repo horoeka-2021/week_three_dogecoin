dbe274a7aff922f9d714b727c90c01e93a1123e0
seshan-b <seshan.b@gmail.com>
2021-10-29 00:43:15 +1300

    Sketched layout

--- routes/auctions.js
+++ routes/auctions.js
@@ -1,4 +1,23 @@
 const express = require('express')
 const router = express.Router()
+
+const path = require('path');
 module.exports = router
 
+
+
+
+// GET /auctions
+router.get('/', (req, res) => {
+    // db.getAllAuctions()
+    //   .then((auctions) => {
+    //     const viewData = {
+    //       auctions: auctions
+    //     }
+    //     res.render('showAuctions', viewData)
+    //     return null
+    //   })
+    //   .catch(err => logError(err, res))
+
+    res.render(path.join(__dirname, "../views/home.hbs"));
+  })
